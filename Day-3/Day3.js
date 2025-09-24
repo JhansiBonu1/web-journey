73fddfb018ecae3f4d6ddb4334c5f91a07b5a5d1
JhansiBonu <jhansibonu1@gmail.com>
2025-09-24 20:50:13 +0530

    Phase0 Day3 completed

---
+++ Day-3/Day3.js
@@ -0,0 +1,15 @@
+let input=document.getElementById("item");
+let btn=document.getElementById("btn");
+let itemlist=document.getElementById("itemlist");
+btn.addEventListener("click",function(){
+    let value=input.value;
+    if(value.trim!==""){
+        let li=document.createElement("li");
+        li.textContent=value;
+        itemlist.appendChild(li);
+        input.value="";
+    }
+    else{
+        alert("Please enter a valid item");
+    }
+});
