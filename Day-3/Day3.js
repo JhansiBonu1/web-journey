205bb5405075a9773ba01c8c32ffa6b659747bdd
JhansiBonu1 <jhansibonu1@gmail.com>
2025-09-24 21:25:30 +0530

    Update Day3.js

--- Day-3/Day3.js
+++ Day-3/Day3.js
@@ -1,15 +1,18 @@
-let input=document.getElementById("item");
-let btn=document.getElementById("btn");
-let itemlist=document.getElementById("itemlist");
-btn.addEventListener("click",function(){
-    let value=input.value;
-    if(value.trim!==""){
-        let li=document.createElement("li");
-        li.textContent=value;
+let input = document.getElementById("item");
+let btn = document.getElementById("btn");
+let itemlist = document.getElementById("itemlist");
+
+btn.addEventListener("click", function () {
+    let value = input.value;
+    if (value.trim() !== "") {   
+        let li = document.createElement("li");
+        li.textContent = value;
+        li.addEventListener("click", function () {
+            itemlist.removeChild(li);
+        });
         itemlist.appendChild(li);
-        input.value="";
-    }
-    else{
+        input.value = "";
+    } else {
         alert("Please enter a valid item");
     }
 });
